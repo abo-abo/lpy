29adfcc590a1655b7333269467239e30efb1cf96
Oleh Krehel <ohwoeowho@gmail.com>
2016-06-06 16:06:25 +0200

    lpy.el: Add license and basic description

--- lpy.el
+++ lpy.el
@@ -1,5 +1,58 @@
 ;;; lpy.el --- A lispy interface to Python -*- lexical-binding: t -*-
 
+;; Copyright (C) 2016 Oleh Krehel
+
+;; Author: Oleh Krehel <ohwoeowho@gmail.com>
+;; URL: https://github.com/abo-abo/lpy
+;; Version: 0.1.0
+;; Keywords: python, lisp
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This is an attempt to implement a variant of `lispy-mode'
+;; (https://github.com/abo-abo/lispy) for Python. Unfortunately,
+;; Python isn't nearly as well-structured as LISP. But Python is
+;; ubiquitous, and the less powerful `lpy-mode' is better than nothing
+;; at all.
+;;
+;; The basic idea of `lpy-mode' is to increase the editing efficiency
+;; by binding useful navigation, refactoring and evaluation commands
+;; to unprefixed keys, e.g. "j" or "e". But only in certain point
+;; positions, so that you are still able to use uprefixed keys to
+;; insert themselves.
+;;
+;; Example, here "|" represents the point position:
+;;
+;;    print |("2+2=%d" % (2 + 2))
+;;
+;; Here, if you press the key "e", the whole line will be evaluated
+;; and "2+2=4" will be printed in the Echo Area. Note that if
+;; `lpy-mode' was off, pressing "e" would instead result in:
+;;
+;;    print e|("2+2=%d" % (2 + 2))
+;;
+;; So inserting any key isn't actually useful with that point position
+;; and e.g. the "e" can be used for evaluating the current statement.
+;;
+;; But, for instance, if you wanted to edit "print" into "printe", you
+;; could do that in a straightforward way, just like you would with
+;; `lpy-mode' off : with "C-b e".
+
 (require 'lispy)
 (require 'jedi nil t)
 (require 'soap)
