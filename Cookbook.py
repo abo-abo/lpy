23e584e842d86ba4e8ec52ef7096758dba7fb693
Oleh Krehel <ohwoeowho@gmail.com>
2019-11-01 17:51:56 +0100

    Cookbook.py (python_deps): Add recipe
    Re #26

--- Cookbook.py
+++ Cookbook.py
@@ -1,10 +1,37 @@
 #* Imports
-from pycook.recipes.emacs import compile
+from pycook.recipes.emacs import byte_compile
+import pycook.insta as st
 
 #* Recipes
-def deps(recipe):
+def python_deps(recipe):
+    st.install_package("libreadline-dev")
+    st.git_clone(
+        "https://github.com/pyenv/pyenv.git",
+        "~/.pyenv/",
+        "38de38e3")
+    st.patch("~/.profile", ['+PATH="$HOME/.pyenv/bin:$PATH"'])
+    st.make(
+        "~/.pyenv/versions/3.6.0",
+        ["pyenv install 3.6.0"])
+    return [
+        'eval "$(pyenv init -)"',
+        "python --version",
+        "pip install --upgrade pip jedi readline",
+        "pip --version"
+    ]
+
+def emacs_deps(recipe):
     packages = ["company", "jedi", "company-jedi", "lispy", "use-package"]
     return "cook :emacs install " + " ".join(packages)
 
 def plain(recipe):
-    return "cook :emacs elpa lpy.el targets/plain.el"
+    st.make(
+        "~/.pyenv/versions/lpy-virtualenv",
+        [
+            "pyenv virtualenv 3.6.0 lpy-virtualenv",
+            "pyenv local lpy-virtualenv"])
+    return [
+        'eval "$(pyenv init -)"',
+        "python --version",
+        "cook :emacs elpa lpy.el targets/plain.el"
+    ]
